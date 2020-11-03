424683751f17bd222470548502520798b5eaf266
ameedasmah <ameed.asmah@hotmail.com>
2020-11-03 19:03:13 +0200

    try

--- client/app.js
+++ client/app.js
@@ -1,2 +1,78 @@
+addEventListener('DOMContentLoaded', () => {
+    console.log('works');
+const fromjason = document.getElementById('jason');
+const subthejason = document.getElementById('sendform');
+const transToCSV = document.getElementById('CSV');
 
+const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+  
+      xhr.responseType = 'json';
+  
+      if (data) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
+  
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      }
+  
+      xhr.onerror = () => {
+        reject('Something went wrong!');
+      }
+  
+      xhr.send(data);
+    })
+    return promise
+  }
+
+
+  
+
+let json = {}
+let csv = ''
+
+const sendData = (json) => {
+    sendHttpRequest('POST', 'http://127.0.0.1:3000/json', json)
+      .then(responseData => {
+        csv = responseData
+        console.log(csv)
+        csvRespond.innerHTML = csv
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+const handeChange = (e) => {
+    json = e.target.value
+    console.log(json)
+    // create(json)
+    
+
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    sendData(json)
+}
+
+
+
+
+
+
+jsonText.addEventListener('change' , handeChange )
+submitJson.addEventListener('click' , handleSubmit)
+
+
+
+
+});
 
--- server.js
+++ server.js
@@ -1,2 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.use(express.static(__dirname +'/./client'))
+app.post('/jason',(req,res)=>{
+    res.status(201).jason(req.body)
+})
+
+app.get('/jason' , (req,res) =>{
+    res.status(200).json(req,res)
+  })
+
+app.listen(3000,()=>{
+    console.log('server is working')
+})
